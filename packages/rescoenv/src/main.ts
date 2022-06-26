87f17bb81c44bcccfb89cd5102d54478f597b368
František Žiačik <ziacik@gmail.com>
2022-06-26 21:14:38 +0200

    add command line arg

--- packages/rescoenv/src/main.ts
+++ packages/rescoenv/src/main.ts
@@ -1,5 +1,6 @@
 import { AzureEnvRetriever } from './app/azure-env-retriever';
 import { env } from 'process';
+import { program } from 'commander';
 
 const token = env.AZURE_PERSONAL_ACCESS_TOKEN;
 
@@ -7,6 +8,15 @@ if (!token) {
 	throw new Error('Please set AZURE_PERSONAL_ACCESS_TOKEN env var.');
 }
 
-const retriever = new AzureEnvRetriever('https://dev.azure.com/resconet', token);
+program.argument('<workitem-id>', 'work item id').parse();
 
-retriever.retrieveFor(19502).then(console.log).catch(console.error);
+program.action((workitemIdStr: string) => {
+	console.log('Retrieving', workitemIdStr);
+	const retriever = new AzureEnvRetriever('https://dev.azure.com/resconet', token);
+	retriever
+		.retrieveFor(+workitemIdStr)
+		.then(console.log)
+		.catch(console.error);
+});
+
+program.parse();
